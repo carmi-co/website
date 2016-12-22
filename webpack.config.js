65adad2a893598eba381380cd424b61cf9005b10
jeantroiani <digitalguest@gmail.com>
2016-12-22 14:30:03 +0000

    added font face

--- webpack.config.js
+++ webpack.config.js
@@ -12,16 +12,20 @@ const config = {
      module: {
         loaders: [
             {
-                test: /\.(png|jpg)$/,
+                test: /\.(woff|woff2|eot|ttf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.(png|jpg|)$/,
                 loader: "url-loader?limit=500000&name=img/img-[hash:6].[ext]"
             },
             {
-            test: /\.less$/,
-            loaders: [
-                'style-loader',
-                'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
-                'less-loader'
-            ]
+                test: /\.less$/,
+                loaders: [
+                    'style-loader',
+                    'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+                    'less-loader'
+                ]
             },
             {
                 test: /\.js$/,
