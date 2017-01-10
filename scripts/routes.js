d537afe747f5306e7d2f219b07396a54f9e1bd2c
jeantroiani <digitalguest@gmail.com>
2017-01-10 15:54:57 +0000

    removed router check

--- scripts/routes.js
+++ scripts/routes.js
@@ -9,7 +9,7 @@ import React from 'react';
 
 function Routes () {
     return (
-        <Router history={browserHistory} onEnter={checkForRedirect}>
+        <Router history={browserHistory}>
             <Route path="/" component={App} >
                 <IndexRoute to="/" component={Home} />        
             </Route>
